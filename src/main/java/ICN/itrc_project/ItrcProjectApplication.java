653e318542e6160c8d118afde89d69eaa2df6826
bricksky <skydh1214@naver.com>
2026-01-23 19:26:17 +0900

    Initial commit

---
+++ src/main/java/ICN/itrc_project/ItrcProjectApplication.java
@@ -0,0 +1,13 @@
+package ICN.itrc_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItrcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItrcProjectApplication.class, args);
+	}
+
+}
